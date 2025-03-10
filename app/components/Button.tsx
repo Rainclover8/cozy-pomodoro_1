dae531e00cfe9b79dd53742d5d9717b78b67ba9f
rainclover8 <ts.baran6161@gmail.com>
2025-03-10 20:39:02 +0300

    position

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -17,7 +17,7 @@ function Button({handleClick, image, className, min}: ButtonProps) {
     <motion.button data-value={min} onClick={handleClick} className={"absolute drop-shadow-2xl shadow-white" + className}
         initial={{scale: 0, x:200, y:-200}}
         animate={{scale: 1, x:0, y:0}}
-        transition={{duration: 0.5}}
+        transition={{duration: 0.7}}
         >
           <Image src={image} width={150} height={150} alt=" Min" className="w-auto h-auto" />
         </motion.button>
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import First from "@/public/25.png";
 import Second from "@/public/15.png";
 import Third from "@/public/5.png";
 import Button from "./components/Button";
+import { motion } from "framer-motion";
 export default function Home() {
   const [value, setValue] = useState(0);
   //  SÜRELER EKLENECEK
@@ -22,13 +23,18 @@ export default function Home() {
     <>
       <div className="overflow-hidden max-h-screen">
         <div className="">
-          <h1 className="text-center text-6xl font-extrabold mt-24 uppercase bg-gradient-to-b from-fuchsia-700 border-t-indigo-500 bg-clip-text text-transparent ">Cozy Pomodoro</h1>
+          <motion.h1
+            className="text-center text-6xl font-extrabold mt-24 uppercase bg-gradient-to-b from-fuchsia-700 border-t-indigo-500 bg-clip-text text-transparent "
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 4 }}
+          >Cozy Pomodoro</motion.h1>
         </div>
 
         <Button image={First} className="" min="4" handleClick={handleClick} />
 
 
-        <Button image={Second} className=" right-20 top-45" min="15" handleClick={handleClick} />
+        <Button image={Second} className=" md:right-20 right-10 top-45" min="15" handleClick={handleClick} />
 
         <Button image={Third} className=" top-1/3 left-1/3" min="5" handleClick={handleClick} />
 
