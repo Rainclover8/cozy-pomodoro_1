1ad56a7f0b89d183829596176309de9858ba0d4d
rainclover8 <ts.baran6161@gmail.com>
2025-03-10 14:03:19 +0300

    ses eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Timer from "./components/Timer";
 import { useState } from "react";
-import First from "../public/25.png";
+import First from "@/public/25.png";
 
 export default function Home() {
   const [value, setValue] = useState(0);
@@ -15,18 +15,17 @@ export default function Home() {
   function handleClick(e: any) {
     const newValue = Number(e.currentTarget.dataset.value);
     setValue(newValue);
-    console.log(newValue);
   }
 
   return (
     <>
       <div className="">
-    <div className="">
-      <h1 className="text-center text-4xl font-bold mt-24 leading-tight uppercase">Cozy Pomodor0</h1>
-    </div>
+        <div className="">
+          <h1 className="text-center text-4xl font-bold mt-24 leading-tight uppercase">Cozy Pomodoro</h1>
+        </div>
 
-        <button data-value="10" onClick={handleClick} className="absolute top-40 left-24 drop-shadow-2xl shadow-white">
-          <Image src={First} width={150} height={150} alt="25 Min" className=" " />
+        <button data-value="1" onClick={handleClick} className="absolute top-40 left-24 drop-shadow-2xl shadow-white">
+          <Image src={First} width={150} height={150} alt="25 Min" className="w-auto h-auto" />
         </button>
       </div>
       {value === 0 ? (
