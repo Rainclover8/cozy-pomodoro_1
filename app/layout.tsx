5490860e28f6e285894c7ef023f36baf5531649b
rainclover8 <ts.baran6161@gmail.com>
2025-03-10 21:36:07 +0300

    origin bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <head>
 
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Indie+Flower&family=Playwrite+HU:wght@100..400&family=Press+Start+2P&display=swap" rel="stylesheet"/>
       </head>
       <body
